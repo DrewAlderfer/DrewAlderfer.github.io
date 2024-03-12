d2ba167699b44514195533c96e74a199716697d6
andrew <andrew@drewalderfer.com>
2024-03-12 07:08:59 -0400

    added javascript post content constructor

--- script.js
+++ script.js
@@ -1,92 +1,53 @@
-// Script to cycle through gallery images
+const feed = document.querySelector(".feed");
+const recents = document.querySelector(".recent");
+const parser = new DOMParser();
+const regex = new RegExp("[a-z_0-9]*\.html", "g");
+const post_info = [];
+const post_title = [];
 
-// Script to open a div with another site inside
-let projects = document.querySelectorAll(".project");
-let panel = document.getElementById("p-expanded")
-let pOpen = false;
-// console.log(panel)
-
-// title case from:
-// https://www.freecodecamp.org/news/three-ways-to-title-case-a-sentence-in-javascript-676a9175eb27/
-//
-function titleCase(str) {
-  str = str.replaceAll("_", " ").split(' ');
-  for (var i = 0; i < str.length; i++) {
-    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-  }
-  return str.join(' ');
-}
-
-// Construct Projects
-projects.forEach (p_post => {
-    let name = p_post.getAttribute("title");
-    // insert image
-    p_post.firstElementChild.innerHTML = `<img src="projects/${name}/images/${name}.png"></img>`;
-    // insert title
-    p_post.getElementsByClassName("p-title").item(0).innerHTML = titleCase(name);
-    // insert tags
-    fetch (`projects/${name}/tags.txt`)
-        .then(x => x.text())
-        .then(y => p_post.lastElementChild.getElementsByClassName("tag").item(0).innerHTML = y)
-    // insert description
-    fetch (`projects/${name}/desc.txt`)
-        .then(x => x.text())
-        .then(y => p_post.lastElementChild.lastElementChild.innerHTML = y)
-})
-// descriptions.forEach( desc => {
-//     name = desc.id
-//     if (name) {
-//         fetch (`projects/${name}/desc.txt`).then(x => x.text()).then(y => desc.innerHTML = y)
-//     }
-// })
-
-// Side Panel Operations
-// Event Listener
-projects.forEach(p => {
-    p.addEventListener("click", sidePanel);
-})
-
-// Sleep Function
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// Open Panel Functions
-function open_panel(panel) {
-    return panel.style.width = "48vw";
-}
-
-// Eventlistener Callback
-function sidePanel() {
-    project = this.getAttribute("title");
-    if (pOpen) {
-        if (this.getAttribute("title") == cur_project) {
-            //you probably want to reload different project hrefs 
-            panel.classList.remove("open")
-            panel.style.width = 0;
-            pOpen = false;
-            panel.innerHTML = ""
-        } else {
-            fetch (`projects/${project}/index.html`).then(x => x.text()).then(y => panel.innerHTML = y)
+function fetchHTMLContent() {
+    fetch("./projects").then((response) => response.text())
+    .then((text) => { return parser.parseFromString(text, 'text/html') })
+    .then((htmlObject) => htmlObject.querySelector('ul').innerText)
+    .then((inner_text) => { 
+        // console.log(inner_text);
+        return inner_text.match(regex);
+    })
+    .then((str_match) => { 
+        // console.log(str_match) 
+        for (const fileName of str_match) {
+            fetch(`./projects/${fileName}`)
+            .then((response) => { 
+                // console.log(`${fileName}`);
+                return response.text();
+            })
+            .then((postString) => { 
+                // console.log(postString);
+                feed.insertAdjacentHTML("beforeend", postString);
+                return parser.parseFromString(postString, 'text/html');
+            })
+            .then((postHtml) => {
+                const info = postHtml.querySelector(".post_info").innerText;
+                const title = postHtml.querySelector(".post_header").innerText;
+                const link = convertToSnakeCase(title);
+                html_element = `<a href="#${link}"><div class="jump_link">\n\t<span class="r_title">${title}</span>\n\t<span class="r_date">${info}</span>\n</div></a>`
+                console.log(html_element);
+                recents.insertAdjacentHTML("beforeend", html_element);
+            });
         }
-
-    } else {
-        open_panel(panel);
-        async function content_fill(){
-            await sleep(1050);
-            fetch (`projects/${project}/index.html`)
-                .then(x => x.text())
-                .then(y => panel.innerHTML = y);
-            pOpen = true;
-            panel.classList.add("open")
-        }
-        content_fill();
-
-        
-        //let thisProject = this.id
-        // panel.innerhtml = thisProject.html
-        // not inner html lol but there's a thing where you can load a page into a div.
-        // you might want to also set a delay.
-    }
-    cur_project = project
+    });
 }
+
+function convertToSnakeCase(titleCaseString) {
+    return titleCaseString.toLowerCase().replace(/\s+/g, '_');
+}
+
+function zip(...arrays) {
+    const length = Math.min(...arrays.map(arr => arr.length));
+    return Array.from({ length }, (_, index) => arrays.map(arr => arr[index]));
+}
+// Usage example:
+fetchHTMLContent();
+
+
+
