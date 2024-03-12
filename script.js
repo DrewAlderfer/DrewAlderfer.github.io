32e37b0f2c51895a29d395e2999d310e7eb59ee2
DrewAlderfer <andrew@drewalderfer.com>
2024-03-12 17:47:02 -0400

    finalized post filtering and styling

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const feed = document.querySelector(".feed");
 const recents = document.querySelector(".r_list");
 const navbutt = document.querySelectorAll(".nav_button");
 const parser = new DOMParser();
-const regex = new RegExp("[a-z_0-9]*\.html", "g");
-const post_info = [];
-const post_title = [];
-const default_tags = ["programming"]
-
+const regex = new RegExp("[A-Za-z_0-9]*\.html", "g");
+const default_tags = ["all"]
 // add eventlistener
 navbutt.forEach(button => {
     button.addEventListener("click", setTags);
@@ -18,19 +15,19 @@ function setTags() {
     fetchHTMLContent(meta);
 }
 
-function fetchHTMLContent(tags) {
-    console.log(tags);
-    recents.innerHTML = "";
-    feed.innerHTML = "";
-    fetch("./projects").then((response) => response.text())
+async function fetchHTMLContent(tags) {
+    feed.innerText = "";
+    recents.innerText = "";
+    const post_list = [];
+    console.log(typeof post_list);
+    return fetch("./projects").then((response) => response.text())
     .then((text) => { return parser.parseFromString(text, 'text/html') })
     .then((htmlObject) => htmlObject.querySelector('ul').innerText)
     .then((inner_text) => { 
-        // console.log(inner_text);
         return inner_text.match(regex);
     })
     .then((str_match) => { 
-        // console.log(str_match) 
+        str_match.sort().reverse();
         for (const fileName of str_match) {
             fetch(`./projects/${fileName}`)
             .then((response) => { 
@@ -42,23 +39,32 @@ function fetchHTMLContent(tags) {
                 return parser.parseFromString(postString, 'text/html');
             })
             .then((postHtml) => {
+                // console.log(postHtml);
                 const meta = getMeta(postHtml);
-                console.log(meta);
                 const check = tags.some((element) => meta.indexOf(element) !== -1);
                 if (check) {
+                    const info = postHtml.querySelector(".post_info").innerText.trim();
+                    const title = postHtml.querySelector(".post_header").innerText.trim();
+                    const id = convertToSnakeCase(title);
+                    postHtml.querySelector(".post_header").setAttribute('id', id);
                     const post = postHtml.querySelector(".post").outerHTML;
-                    const info = postHtml.querySelector(".post_info").innerText;
-                    const title = postHtml.querySelector(".post_header").innerText;
-                    const link = convertToSnakeCase(title);
-                    html_element = `<a href="#${link}"><div class="jump_link">\n\t<span class="r_title">${title}</span>\n\t<span class="r_date">${info}</span>\n</div></a>`
-                    // console.log(html_element);
+                    html_element = `<a href="#${id}"><div class="jump_link">\n\t<span class="r_title">${title}</span>\n\t<span class="r_date">${info}</span>\n</div></a>`
                     feed.insertAdjacentHTML("beforeend", post);
                     recents.insertAdjacentHTML("beforeend", html_element);
+                    // post_list.push([info, title, post, html_element]);
                 }
             });
-        }
+        };
+        // return post_list;
     });
 }
+
+// async function sort_posts(tags) {
+//     const posts = await fetchHTMLContent(tags);
+//     posts.forEach((item) => {
+//         console.log(item);
+//     });
+// }
 
 function convertToSnakeCase(titleCaseString) {
     return titleCaseString.toLowerCase().replace(/\s+/g, '_');
@@ -69,6 +75,10 @@ function getMeta(htmlList) {
 }
 // Usage example:
 fetchHTMLContent(default_tags);
+// sort_posts(default_tags);
+
+
+
 
 
 
