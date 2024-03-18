14962ea3bb6309e14779c49ef686b5f5fcb92727
DrewAlderfer <drew@drewalderfer.com>
2024-03-18 18:30:31 -0400

    added photos and refined styling

--- script.js
+++ script.js
@@ -1,36 +1,82 @@
 const feed = document.querySelector(".feed");
 const recents = document.querySelector(".r_list");
-const navbutt = document.querySelectorAll(".nav_button");
+const nav = document.querySelector(".nav");
 const parser = new DOMParser();
 const regex = new RegExp("[A-Za-z_0-9]*\.html", "g");
-const default_tags = ["all"];
+const default_args = [["all"], true];
 var post;
 // add eventlistener
-navbutt.forEach(button => {
-    button.addEventListener("click", setTags);
-})
 
-
-function setTags() {
-    const tag = parser.parseFromString(this.innerHTML, 'text/html');
-    const meta = getMeta(tag);
-    fetchHTMLContent(meta);
+async function toggle_filter(item) {
+    if (item.getAttribute("status") == "ON") {
+        item.setAttribute("status", "OFF");
+        item.setAttribute("style", "color: var(--black)");
+    } else {
+        item.setAttribute("status", "ON");
+        item.setAttribute("style", "color: var(--hl)");
+    }
+    return true
 }
 
-async function fetchHTMLContent(tags) {
+async function setTags() {
+    const toggle = await toggle_filter(this);
+    let meta = [];
+    // const tag = await parser.parseFromString(this.innerHTML, 'text/html');
+    const nodes = await document.querySelectorAll('.nav_button');
+    for await (node of nodes) {
+        if (node.getAttribute('status') == "ON") {
+            meta = meta.concat(getMeta(node));
+        }
+    }
+    console.log(meta);
+    // console.log(nodes.filter((node) => node.getAttribute('status') == "ON"));
+    // const meta = getMeta(tag);
+    fetchHTMLContent([meta, false]);
+}
+
+async function make_filters(tags) {
+    tags.sort();
+    result = "";
+    for await (tag of tags) {
+        // console.log(tag);
+        const id = await convertToSnakeCase(tag);
+        nav_button = `<div class="nav_button" id="${id}" status="OFF">${tag}
+                            <div class="meta">
+                                <ul>
+                                    <li>${tag}</li>
+                                </ul>
+                            </div>
+                        </div>`
+        result = result + nav_button;
+    }
+    return result
+}
+
+async function fill_buffers(args) {
+    const tags = args[0];
+    const setting = args[1];
     const response = await fetch("projects/");
     const text = await response.text();
     const html_object = await parser.parseFromString(text, 'text/html');
     const file_names = await html_object.querySelectorAll('li');
     let post_buffer = "";
     let link_buffer = "";
+    let filter_options = [];
     for await (file of file_names) {
         if (file.innerText.includes(".html", -4)) {
             const p_response = await fetch("projects/" + file.innerText);
             const p_text = await p_response.text();
             const post_html = await parser.parseFromString(p_text, 'text/html');
             const meta = await getMeta(post_html);
-            const check = tags.some((element) => meta.indexOf(element) !== -1);
+            // console.log(meta);
+            if (setting) {
+                for await (tag of meta) {
+                    if (filter_options.includes(tag) !== true && tag !== "all") {
+                        filter_options.push(tag);
+                    };
+                };
+            };
+            const check = tags.every((element) => meta.indexOf(element) !== -1);
             if (check) {
                 const info = await post_html.querySelector(".post_info").innerText.trim();
                 const title = await post_html.querySelector(".post_header").innerText.trim();
@@ -43,11 +89,32 @@ async function fetchHTMLContent(tags) {
             }
         }
     }
+    const filter_buffer = await make_filters(filter_options);
+    return [post_buffer, link_buffer, filter_buffer]
+}
+
+
+async function fetchHTMLContent(args) {
+    const buffers = await fill_buffers(args);
+    const link_buffer = await buffers[1];
+    const post_buffer = await buffers[0];
+    const filter_buffer = await buffers[2];
+    if (args[1]) {
+        nav.innerHTML = "";
+        nav.insertAdjacentHTML("afterbegin", filter_buffer);
+    }
+
     feed.innerHTML = "";
     recents.innerHTML = "";
     recents.insertAdjacentHTML("afterbegin", link_buffer);
     feed.insertAdjacentHTML("afterbegin", post_buffer);
     const buttons = await document.querySelectorAll(".button");
+    const navbutt = await document.querySelectorAll(".nav_button");
+
+    await navbutt.forEach(button => {
+        button.addEventListener("click", setTags);
+    })
+
     await buttons.forEach((button) => button.addEventListener("click", (event) => {
         if (event.target.getAttribute("status") == "ON") {
             event.target.setAttribute("status", "OFF");
@@ -69,6 +136,6 @@ function getMeta(htmlList) {
     return result;
 }
 // Usage example:
-fetchHTMLContent(default_tags);
+fetchHTMLContent(default_args);
 // sort_posts(default_tags);
 
